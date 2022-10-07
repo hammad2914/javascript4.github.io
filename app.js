c865611a06bfb1d8855339466a5c345872c26e49
Hammad <ch.hammad2914@gmail.com>
2022-10-07 22:18:11 +0500

    java4

---
+++ app.js
@@ -0,0 +1,36 @@
+function image(){
+    var image=document.getElementById("first").style.display="none"
+    var image1=document.getElementById("second").style.display="block"
+     image=image1
+
+}
+
+function image1(){
+    var image=document.getElementById("first").style.display="block"
+    var image1=document.getElementById("second").style.display="none"
+     image=image1
+}
+
+function image2(){
+    var image=document.getElementById("third").style.display="block"
+    var image1=document.getElementById("second").style.display="none"
+     image=image1
+}
+
+function image3(){
+    var image=document.getElementById("third").style.display="none"
+    var image1=document.getElementById("second").style.display="block"
+     image=image1
+}
+
+function image4(){
+    var image=document.getElementById("first").style.display="none"
+    var image1=document.getElementById("third").style.display="block"
+     image=image1
+}
+
+function image5(){
+    var image=document.getElementById("third").style.display="none"
+    var image1=document.getElementById("first").style.display="block"
+     image=image1
+}
